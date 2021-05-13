35645d8862c67576b8ca72291a4b64abc0a674ad
Ariana Nayeli Villalva <arianavillava@upeu.edu.pe>
2021-05-13 15:58:52 -0500

    first commit

---
+++ js/scrpt.js
@@ -0,0 +1,47 @@
+var nombre = document.getElementById("nombre");
+var fila = "";
+var est = 0;
+function registrar(){
+    if(est == 0){
+    var table = document.getElementById("table");
+    var tbody = document.getElementById("tbd");
+
+    //crear fila
+    var fil = document.createElement("tr");
+
+    //crear celda
+    var celda_titulo = document.createElement("td");
+    celda_titulo.innerHTML=titulo.value;
+    var celda_contenido = document.createElement("td");
+    celda_contenido.innerHTML=cotenido.value;
+    var celda_accion = document.createElement("td");
+    celda_accion.classList.add("accion");
+    celda_accion.innerHTML=
+    "<a href='#' class='icono2' onclick='editar(this)'>editar</a>"
+    +"<a href='#' class='icono1' onclick='eliminar(this)'>eliminar</a>"
+    fil.appendChild(celda_nombre);
+    fil.appendChild(celda_contenido);
+    fil.appendChild(celda_accion);
+    tbody.appendChild(fil);
+    table.appendChild(tbody);
+    limpiar();
+    }else{
+        fila.cells[0].innerHTML = nombre.value;
+        fila = "";
+        limpiar();
+        estado = 0;
+    }
+}
+function limpiar(){
+    nombre.value = "";
+    nombre.focus();
+}
+function editar(row){
+    fila = row.parentNode.parentNode;
+    nombre.value = fila.cells[0].innerHTML;
+    est = 1;
+}
+function eliminar(row){
+    let f = row.parentNode.parentNode;
+    f.parentNode.removeChild(f);
+}
